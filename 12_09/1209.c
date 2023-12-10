e23b76f02513ab56aea8c1d0f28b99bae026b94d
AAAwangh <15594541219@163.com>
2023-12-10 11:28:56 +0800

    .\1209.c

--- 12_09/1209.c
+++ 12_09/1209.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-int main()
+int main(int argc,char*argv[])
 {
-    printf("aaa");
+    int count;
+    printf("the command line has %darguments:\n",argc-1);
+    for(count =1;count<argc;count++)
+    {
+        printf("%d: %s\n",count,argv[count]);
+    }
+    printf("\n");
     return 0;
 }
